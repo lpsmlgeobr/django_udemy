103def0fa235edcb94695d4c0295b6a1bbe3c713
= <lpsmlgeobr@gmail.com>
2021-09-22 09:05:17 -0300

    dynamic urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+def months(request, month):
+    dic = {"january":"vem que vem", "february": "Teve bom", "march":"nois"}
+    if month in dic.keys():
+        return HttpResponse(dic[month])
+    else:
+        return HttpResponseNotFound("Tem esse mês não!")
+
